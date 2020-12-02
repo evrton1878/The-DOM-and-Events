9389716e9f6fcf0e5a9f939b0e5c26750fe6bd67
Boyan Vasilev <vasilev1878@gmail.com>
2020-12-02 10:29:00 -0600

    script file

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ function init () {
     paragraph = document.querySelector("p");
 
     // Put your code for the exercises here.
+    button.addEventListener("click", function () {
+      paragraph.innerHTML="Houston, we have lift off!";
+          });
+          missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.backgroundColor = "red";
+    });
+     missionAbort.addEventListener("mouseout", function() {
+         missionAbort.style.backgroundColor="" ;
+     });
+    missionAbort.addEventListener("click", function () {
+      let responce =confirm("Are you sure you want to abort the mission?");;
+          });
+   
 }
 
 window.onload = init;
